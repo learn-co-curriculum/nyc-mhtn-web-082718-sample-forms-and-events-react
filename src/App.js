53dbd13f3573e066a8ec517cb272f4f5ea0249ad
curiositypaths <curiositypaths@users.noreply.github.com>
2018-11-01 07:10:52 -0400

    Sample forms and events code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-class App extends Component {
+export class CountWords extends React.Component {
   render() {
+    //debugger;
+    const numberOfWords = this.props.data.split(" ").filter(word => !!word)
+      .length;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <h1 onClick={this.props.onClick}>
+          This sentence includes {numberOfWords} words
+          {this.props.children}
+        </h1>
+      </>
     );
   }
 }
 
-export default App;
+export const CountWordsExceptPizza = props => {
+  const numberOfWords = props.data
+    .split(" ")
+    .filter(word => !!word)
+    .filter(word => word !== "pizza").length;
+  return (
+    <h1>This sentence includes {numberOfWords} words (that are not Pizza)</h1>
+  );
+};
+
+let saveReferenceToEvent = null;
+
+
+
+  myFunctionalComponent() {
+    return (
+      <>
+        {/* <input
+          onChange={this.handleInputChanges}
+          type="text"
+          value={this.state.words}
+        />
+        <CountWords onClick={this.addPizzas} data={this.state.words}>
+          <CountWordsExceptPizza data={this.state.words} />
+        </CountWords> */}
+        <h1 onClick={props.eventHandler}>Explore events</h1>
+      </>
+    );
+  }
+
